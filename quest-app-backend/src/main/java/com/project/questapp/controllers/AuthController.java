01f9bfc7bb9b39a0b471527ac2454cd8f2fafafe
Kim Jeongho <sldh12@daum.net>
2023-03-27 18:01:10 +0900

    스프링 시큐리티 추가

---
+++ quest-app-backend/src/main/java/com/project/questapp/controllers/AuthController.java
@@ -0,0 +1,58 @@
+package com.project.questapp.controllers;
+
+import com.project.questapp.entities.User;
+import com.project.questapp.requests.UserRequest;
+import com.project.questapp.security.JwtTokenProvider;
+import com.project.questapp.services.UserService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/auth")
+public class AuthController {
+
+    private AuthenticationManager authenticationManager;
+
+    private JwtTokenProvider jwtTokenProvider;
+
+    private UserService userService;
+
+    private PasswordEncoder passwordEncoder;
+
+    public AuthController(AuthenticationManager authenticationManager, JwtTokenProvider jwtTokenProvider, UserService userService, PasswordEncoder passwordEncoder) {
+        this.authenticationManager = authenticationManager;
+        this.jwtTokenProvider = jwtTokenProvider;
+        this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @PostMapping("/loing")
+    public String login(@RequestBody UserRequest userRequest) {
+        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(userRequest.getUserName(), userRequest.getPassword());
+        Authentication auth = authenticationManager.authenticate(authToken);
+        SecurityContextHolder.getContext().setAuthentication(auth);
+        String jwtToken = jwtTokenProvider.generateJwtToken(auth);
+        return "Bearer " + jwtToken;
+    }
+
+    @PostMapping("/register")
+    public ResponseEntity<String> register(@RequestBody UserRequest userRequest) {
+        if(userService.getOneUserByUserName(userRequest.getUserName()) != null) {
+            return new ResponseEntity<>("Username already in use", HttpStatus.BAD_REQUEST);
+        }
+        User user = new User();
+        user.setUserName(userRequest.getUserName());
+        user.setPassword(passwordEncoder.encode(userRequest.getPassword()));
+        userService.createUser(user);
+        return new ResponseEntity<>("User successfully registered", HttpStatus.CREATED);
+    }
+}
--- quest-app-backend/src/main/java/com/project/questapp/services/UserService.java
+++ quest-app-backend/src/main/java/com/project/questapp/services/UserService.java
@@ -44,4 +44,8 @@ public class UserService {
     public void deleteOneUser(Long userId) {
         userRepository.deleteById(userId);
     }
+
+    public User getOneUserByUserName(String userName) {
+        return userRepository.findByUserName(userName);
+    }
 }
