ce86944e20165c94eaa9bcd376cf94a0c550e11f
Kim Jeongho <sldh12@daum.net>
2023-04-13 17:25:53 +0900

    프로필 이미지 선택 테스트

--- quest-app-frontend/src/components/Avatars/Avatar.js
+++ quest-app-frontend/src/components/Avatars/Avatar.js
@@ -7,17 +7,32 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { Modal } from '@material-ui/core';
+import {
+  ListItem,
+  List,
+  ListItemSecondaryAction,
+  Radio,
+} from '@material-ui/core';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
     margin: 50,
   },
+  modal: {
+    display: 'flex',
+    maxWidth: 200,
+  },
 });
 
-const Avatar = () => {
+const Avatar = (props) => {
   const classes = useStyles();
-  const [open, setOpen] = useState();
+  const [open, setOpen] = useState(false);
+  const [selectedValue, setSelectedValue] = useState(0);
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
+  };
 
   const handleOpen = () => {
     setOpen(true);
@@ -33,7 +48,7 @@ const Avatar = () => {
         <CardMedia
           component='img'
           alt='User Avatar'
-          image='/avatars/avatar0.png'
+          image={`/avatars/avatar${selectedValue}.png`}
           title='User Avatar'
         />
         <CardContent>
@@ -51,12 +66,37 @@ const Avatar = () => {
         </CardActions>
       </Card>
       <Modal
+        className={classes.modal}
         open={open}
         onClose={handleClose}
         aria-labelledby='simple-modal-title'
         aria-describedby='simple-modal-description'
       >
-        <div>Modal Test</div>
+        <List dense>
+          {[1, 2, 3, 4, 5, 6].map((key) => {
+            const labelId = `checkbox-list-secondary-label-${key}`;
+            return (
+              <ListItem key={key}>
+                <CardMedia
+                  style={{ maxWidth: 100 }}
+                  component='img'
+                  alt={`Avatar n°${key}`}
+                  image={`/avatars/avatar${key}.png`}
+                  title='User Avatar'
+                />
+                <ListItemSecondaryAction>
+                  <Radio
+                    edge='end'
+                    value={key}
+                    onChange={handleChange}
+                    checked={'' + selectedValue === '' + key}
+                    inputProps={{ 'aria-labelledby': labelId }}
+                  />
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
       </Modal>
     </div>
   );
